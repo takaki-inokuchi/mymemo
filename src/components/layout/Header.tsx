babc8944629699e648637794e7c604d8daa3debd
井ノ口 孝輝 <inokutitakaki@yahoo.co.jp>
2025-10-24 15:43:59 +0900

    修正ヘッド

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='fixed left-0 w-full px-6 py-4 text-2xl font-bold text-white bg-pink-300'>
+      メモアプリ!
+    </header>
+  )
+}
+
+export default Header
